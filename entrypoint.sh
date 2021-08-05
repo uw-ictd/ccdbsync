ce019c607b4c34ee205c3c2c6afe8c355684ef10
clarlars <clarlars@gmail.com>
2021-08-05 12:08:20 -0700

    Use environment variables if they have been set properly as config values

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,49 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env"
 
+# Use environmental variables in config.txt file if they are available
+if [[ -n "${POSTGRES_URL}" ]]; then
+  sed -i '1d' config/config.txt
+  sed -i "1 i ${POSTGRES_URL}" config/config.txt
+fi
+if [[ -n "${POSTGRES_USER}" ]]; then
+  sed -i '2d' config/config.txt
+  sed -i "2 i ${POSTGRES_USER}" config/config.txt
+fi
+if [[ -n "${POSTGRES_PASSWORD}" ]]; then
+  sed -i '3d' config/config.txt
+  sed -i "3 i ${POSTGRES_PASSWORD}" config/config.txt
+fi
+if [[ -n "${ODK_SERVER}" ]]; then
+  sed -i '4d' config/config.txt
+  sed -i "4 i ${ODK_SERVER}" config/config.txt
+fi
+if [[ -n "${ODK_APP_ID}" ]]; then
+  sed -i '5d' config/config.txt
+  sed -i "5 i ${ODK_APP_ID}" config/config.txt
+fi
+if [[ -n "${ODK_USERNAME}" ]]; then
+  sed -i '6d' config/config.txt
+  sed -i "6 i ${ODK_USERNAME}" config/config.txt
+fi
+if [[ -n "${ODK_PASSWORD}" ]]; then
+  sed -i '7d' config/config.txt
+  sed -i "7 i ${ODK_PASSWORD}" config/config.txt
+fi
+if [[ -n "${LOG_DATA_DIR}" ]]; then
+  sed -i '8d' config/config.txt
+  sed -i "8 i ${LOG_DATA_DIR}" config/config.txt
+fi
+if [[ -n "${DEFAULT_TIMEZONE}" ]]; then
+  sed -i '9d' config/config.txt
+  sed -i "9 i ${DEFAULT_TIMEZONE}" config/config.txt
+fi
+if [[ -n "${LOG_TIMEZONE}" ]]; then
+  sed -i '10d' config/config.txt
+  sed -i "9 a ${LOG_TIMEZONE}" config/config.txt
+fi
+
+
 echo "00 * * * * /syncCCDB.sh
 # Don't remove the empty line at the end of this file. It is required to run the cron job" > scheduler.txt
 
